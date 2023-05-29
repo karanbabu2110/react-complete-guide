501a024f9d04cac0ba61469cbeacb5b53652dbfa
KARAN BABU B <38027135+karanbabu2110@users.noreply.github.com>
2023-05-29 21:40:26 +0530

    Formatting

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,14 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
-
-export default function ExpenseItem(props) { //we can give any name instead props
+export default function ExpenseItem(props) {
+  //we can give any name instead props
 
   return (
     <div className="expense-item">
-        <ExpenseDate date = {props.date}/>
-        <div className="expense-item__description">
-            <h2>{props.title}</h2>
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>
