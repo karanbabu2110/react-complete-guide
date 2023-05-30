837af2a465ddf9cb43251a525f0be68bd1692b90
KARAN BABU B <38027135+karanbabu2110@users.noreply.github.com>
2023-05-30 20:21:31 +0530

    Working with Multiple States

--- src/components/NewExpenses/ExpenseFrom.js
+++ src/components/NewExpenses/ExpenseFrom.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React,{useState} from "react";
 
 import "./ExpenseForm.css";
 //We can use either function or const only our preference
 const ExpenseForm = () => {
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
     // Vanilla JavaScript
     //document.getElementById('').addEventListener('click', (event) => {})
 
     const titleChangeHandler = (event) => { //event - default constant for any event to be hanbdled
-        console.log(event.target.value);
-    }
+        setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value);
+    };
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.target.value);
+    };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" minLength={2} onInput={titleChangeHandler}/>
+          <input type="text" minLength={2} onChange={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-01-01" />
+          <input type="date" min="2019-01-01" max="2022-01-01" onChange={dateChangeHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
