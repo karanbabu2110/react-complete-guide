0f0a5e8615b1469e045a635fe12156f8e94619c3
KARAN BABU B <38027135+karanbabu2110@users.noreply.github.com>
2023-05-30 15:46:23 +0530

    State - useState helps to re-render the specific component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,22 @@
+import React, {useState} from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 
 export default function ExpenseItem(props) {
-  //we can give any name instead props
+  //we can give any name instead props 
+  const [title, setTitle] = useState(props.title);
+  //First element{title} for which value we are changing.
+  // Second Element{setTitle} is a function to change value.
   const clickHandler = () => {
-    console.log('clicked!!');
+    setTitle('Updated!!');
   };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change Title</button>
