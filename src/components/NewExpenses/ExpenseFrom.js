34a4eaa40d8bebfc5699c655305aa3303a6267a4
KARAN BABU B <38027135+karanbabu2110@users.noreply.github.com>
2023-05-30 20:07:54 +0530

    Listening to User Input.

--- src/components/NewExpenses/ExpenseFrom.js
+++ src/components/NewExpenses/ExpenseFrom.js
@@ -3,12 +3,18 @@ import React from "react";
 import "./ExpenseForm.css";
 //We can use either function or const only our preference
 const ExpenseForm = () => {
+    // Vanilla JavaScript
+    //document.getElementById('').addEventListener('click', (event) => {})
+
+    const titleChangeHandler = (event) => { //event - default constant for any event to be hanbdled
+        console.log(event.target.value);
+    }
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" minLength={2} />
+          <input type="text" minLength={2} onInput={titleChangeHandler}/>
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
