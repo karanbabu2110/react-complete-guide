b7a59f7e7d22dffcf2f22496a07261b4b963d5e8
KARAN BABU B <38027135+karanbabu2110@users.noreply.github.com>
2023-05-30 18:28:27 +0530

    Adding Form Inputs

---
+++ src/components/NewExpenses/ExpenseFrom.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./ExpenseForm.css";
+//We can use either function or const only our preference
+const ExpenseForm = () => {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input type="text" minLength={2} />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01" />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="date" min="2019-01-01" max="2022-01-01" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
